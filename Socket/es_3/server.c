7bf4ba5a15bcc8ed02d992bc3d8838f10ed9645f
FMarck07 <150336290+FMarck07@users.noreply.github.com>
2025-11-07 22:36:06 +0100

    Update server.c

--- Socket/es_3/server.c
+++ Socket/es_3/server.c
@@ -1,37 +1,20 @@
 /*
 Esercizio 3
-Scrivere il codice in C, di un applicazione Socket CLIENT-SERVER in cui il server riceve in input 1 stringa,
-e dopo aver effettuato gli eventuali ed opportuni controlli (se necessari), rispedisce al Client la stringa
-ordinata alfabeticamente (eliminando i caratteri speciali).
+Scrivere il codice in C di un'applicazione Socket CLIENT-SERVER in cui il server riceve in input 1 stringa
+e, dopo gli opportuni controlli, rispedisce al Client la stringa ordinata alfabeticamente.
 */
 
-// SERVER
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
-#include <netdb.h>
 #include <string.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <errno.h>
 #include <ctype.h>
 #include <unistd.h>
 
 #define DIM 100
 #define SERVERPORT 1313
-
-// Funzione per eliminare i caratteri speciali, mantenendo solo lettere e numeri
-void pulisci_stringa(char *dest, const char *src) {
-    int j = 0;
-    for (int i = 0; src[i] != '\0'; i++) {
-        if (isalnum((unsigned char)src[i])) {
-            dest[j++] = src[i];
-        }
-    }
-    dest[j] = '\0';
-}
 
 // Funzione per ordinare alfabeticamente una stringa (bubble sort)
 void ordina_stringa(char *str) {
@@ -47,23 +30,21 @@ void ordina_stringa(char *str) {
     }
 }
 
-int main()
-{
-    struct sockaddr_in servizio, addr_remoto;
-    servizio.sin_family = AF_INET;
-    servizio.sin_addr.s_addr = htonl(INADDR_ANY);
-    servizio.sin_port = htons(SERVERPORT);
-
+int main() {
     int socketfd, soa;
+    struct sockaddr_in servizio, addr_remoto;
     socklen_t fromlen = sizeof(addr_remoto);
     char str[DIM];
-    char pulita[DIM];
 
     socketfd = socket(AF_INET, SOCK_STREAM, 0);
     if (socketfd < 0) {
         perror("Errore creazione socket");
         exit(1);
     }
+
+    servizio.sin_family = AF_INET;
+    servizio.sin_addr.s_addr = htonl(INADDR_ANY);
+    servizio.sin_port = htons(SERVERPORT);
 
     if (bind(socketfd, (struct sockaddr*)&servizio, sizeof(servizio)) < 0) {
         perror("Errore nel bind");
@@ -72,26 +53,25 @@ int main()
     }
 
     listen(socketfd, 10);
+    printf("Server avviato. In ascolto sulla porta %d...\n", SERVERPORT);
 
     for (;;) {
-        printf("\nServer in ascolto...\n");
-        fflush(stdout);
-
         soa = accept(socketfd, (struct sockaddr*)&addr_remoto, &fromlen);
         if (soa < 0) {
-            perror("Errore");
+            perror("Errore nella accept");
+            continue;
         }
 
-        read(soa, str, sizeof(str));
-        printf("Stringa ricevuta: %s\n", str);
+        memset(str, 0, DIM);
+        read(soa, str, DIM);
 
-        pulisci_stringa(pulita, str);
+        printf("\nStringa ricevuta: %s\n", str);
 
-        ordina_stringa(pulita);
+        ordina_stringa(str);
 
-        printf("Stringa pulita e ordinata: %s\n", pulita);
+        printf("Stringa ordinata: %s\n", str);
 
-        write(soa, pulita, sizeof(pulita));
+        write(soa, str, strlen(str) + 1);
 
         close(soa);
     }
