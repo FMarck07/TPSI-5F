21100676b24aaa04cd01f25ee8eb2878544be868
FMarck07 <150336290+FMarck07@users.noreply.github.com>
2025-12-01 20:51:55 +0100

    Refactor socket server and client implementation

--- Socket/es_9/server.c
+++ Socket/es_9/server.c
@@ -1,65 +1,45 @@
-/*Scrivere il codice in C di un’applicazione socket CLIENT–SERVER in cui il server riceve in input una stringa e, dopo aver effettuato gli opportuni controlli (se necessari), rispedisce al client il numero totale di parole presenti nella stringa.
-*/
+//CLIENT
+#include <ctype.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-// SERVER
-#include <stdio.h>      //std in-out
-#include <stdlib.h>     //per utilizzo di certe funzioni:htonl,rand,....
-#include <sys/socket.h> //funz. accept+bind+listen
-#include <sys/types.h>  //funz. accept
-#include <netinet/in.h> //definiscono la struttura degli indirizzi
-#include <netdb.h>      //strutture hostent e servent che identificano l'host tramite iol nome
-#include <string.h>     //funz. stringhe
-#include <fcntl.h>      //descrittore di file
-#include <signal.h>     //consente l'utilizzo delle funzioni per la gestione dei segnali fra processi
-#include <errno.h>      //gestioni errori connessione
-#include <ctype.h>      //bind
-#include <unistd.h>     // file header che consente l'accesso alle API dello standard POSIX
 #define SERVERPORT 1450
 
-int ContaParole(char stringa[]){
-    int count = 1;
-    for(int i = 0; i<strlen(stringa); i++){
-        if(stringa[i] == ' ' || stringa[i] == '\n'){
-            count++;
-        }
-    }
-    return count;
-}
+//Client: Socket, connect
 
-int main(){
-    char stringa[256];
-    struct sockaddr_in servizio;
+int main()
+{
+    
+    struct sockaddr_in servizio; 
+    int socketfd, numeroParole;
+    char str[256];
     servizio.sin_family = AF_INET;
     servizio.sin_addr.s_addr = htonl(INADDR_ANY);
     servizio.sin_port = htons(SERVERPORT);
-    int socketfd, soa, froml = sizeof(servizio), parole = 0;
+    
     if((socketfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-        printf("Chiamata fallita alla system call socket");
-        exit(0);
+        printf("Chiamata fallita");
+        exit(1);
     }
-    if(bind(socketfd, (struct sockaddr*) &servizio, froml) == -1){
-        printf("Chiamata fallita alla system call bind");
-        exit(0);
+    
+    if(connect(socketfd, (struct sockaddr*)&servizio, sizeof(servizio)) == -1){
+        printf("Chiamata fallita");
+        exit(1);
     }
-    if(listen(socketfd, 10) == -1){
-        printf("Chiamata fallita alla system call listen");
-        exit(0);
-    }
-    for(; ;){
-        printf("\nserver in attesa.. \n");
-        fflush(stdout);
-        if((soa = accept(socketfd, (struct sockaddr*) &servizio, &froml)) == -1){
-            printf("Chiamata fallita alla system call accept");
-            exit(0);
-        }
-        read(soa, stringa, sizeof(stringa));
-        printf("Ricevuto %s\n", stringa);
-        parole = ContaParole(stringa);
-        printf("Numero di parole presenti nella stringa: %d", parole);
-        write(soa, &parole, sizeof(parole));
-        close(soa);
-    }
-    close(socketfd);
+    
+    printf("Inserisci una stringa: ");
+    fgets(str, sizeof(str), stdin);
+    write(socketfd, str, strlen(str) + 1); 
 
+    read(socketfd, &numeroParole, sizeof(int));
+    printf("Numero di parole nella stringa inviata: %d\n", numeroParole);
+    close(socketfd);
+    
     return 0;
 }
